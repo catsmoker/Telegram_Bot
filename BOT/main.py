12724fcefc22b68769faa9ad69d854cf71aa7bdd
EXCITED-TO-SEE <119059457+EXCITED-TO-SEE@users.noreply.github.com>
2023-01-11 20:17:34 +0100

    Update main.py

--- BOT/main.py
+++ BOT/main.py
@@ -12,6 +12,11 @@ from googletrans import Translator
 BOT_TOKEN = config("BOT_TOKEN")
 bot= telebot.TeleBot(BOT_TOKEN)
 
+weather = ["weather","temp","temprature"]
+greetings = ["hello","hi","hey"]
+whoAreYou = ["who","what"]
+botName = "WAGNIMN"
+
 bot_data={
     "name" : ["wagnimn"]
 }
@@ -95,6 +100,25 @@ def handleOffensiveMessage(message):
 @bot.message_handler(commands=["start"])
 def startBot(message):
     bot.send_message(message.chat.id,text_messages["welcome"])
+
+#answering every message not just commands
+def isMSg(message):
+    return True
+
+
+@bot.message_handler(func=isMSg)
+def reply(message):
+    words = message.text.split()
+    if words[0].lower() in weather :
+        report = getCurrentWeather()
+        return bot.send_message(message.chat.id,report or "failed to get weather !!")
+    if words[0].lower() in whoAreYou :
+        return bot.reply_to(message,f"i am {botName}")
+    if words[0].lower() in greetings :
+        return bot.reply_to(message,"hey how is going!")
+    else:
+        return bot.reply_to(message,"that's not a command of mine!")
+
 
 #* saying Welcome to joined members
 #* saying goodbye to left members
